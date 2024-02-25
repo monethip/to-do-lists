6a9f2a45052e8b163a770c2e434261d769771ed9
monethip <monethip1299@gmail.com>
2024-02-25 17:43:10 +0700

    loading

--- src/app/(primary)/page.tsx
+++ src/app/(primary)/page.tsx
@@ -12,8 +12,8 @@ export default function MainPage() {
     const dispatch = useDispatch();
     const router = useRouter();
     const [currentPage, setCurrentPage] = useState(1);
-    const [skip, setSkip] = useState(0)
-    const [Loading, SetLoading] = useState(true)
+    const [skip, setSkip] = useState(0);
+    const [loading, setLoading] = useState(true);
 
     const { data } = useSelector((state) => state.todolists);
 
@@ -24,7 +24,9 @@ export default function MainPage() {
         const newSkip = (pageNumber - 1) * limit;
         setSkip(newSkip);
         setCurrentPage(pageNumber);
-        dispatch(getResources({ limit, skip: skip }));
+        dispatch(getResources({ limit, skip: newSkip })).then(() => {
+            setLoading(false);
+        });
         const url = `?limit=${limit}&skip=${skip}`;
         router.push(url);
         // const newParams = new URLSearchParams();
@@ -41,21 +43,23 @@ export default function MainPage() {
         router.push(`/to-do/${id}`)
     }
 
-    // if (!data) {
-    //     return 'Loading...';
-    // }
+    function Loading() {
+        return <div>loading...</div>;
+    }
 
     return (
         <div className='max-w-screen-xl mx-auto p-4 flex flex-col items-center min-h-screen'>
-            <div className='my-8 text-xl'>Your Tasks</div>
-            <hr />
-            <div className="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-4 mb-10">
-                {data?.todos?.map((item, index) => (
-                    <div onClick={(() => handleToDoDetail(item.id))} className={getStatus(item.completed).textColor} key={index}>{item.todo}</div>
-                ))}
-            </div>
-
-            <Pagination currentPage={currentPage} skip={skip} limit={limit} total={total} paginate={paginate} />
+            {loading ? <Loading /> :
+                <>
+                    <div className='my-8 text-xl'>Your Tasks</div>
+                    <div className="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-4 mb-10">
+                        {data?.todos?.map((item, index) => (
+                            <div onClick={(() => handleToDoDetail(item.id))} className={getStatus(item.completed).textColor} key={index}>{item.todo}</div>
+                        ))}
+                    </div>
+                    <Pagination currentPage={currentPage} skip={skip} limit={limit} total={total} paginate={paginate} />
+                </>
+            }
         </div>
     )
 }
--- src/app/(primary)/to-do/[id]/page.tsx
+++ src/app/(primary)/to-do/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import withAuth from "@/components/withAuth";
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from '@/redux/store';
 import { getOneToDoListResources, updateToDoListResources } from "@/redux/todolist";
 import { usePathname, useRouter } from 'next/navigation';
@@ -13,23 +13,32 @@ const ToDoList = () => {
     const router = useRouter();
 
     const id = pathName?.split("/").pop();
+
+    const [loading, setLoading] = useState(true);
     const { todoTask } = useSelector((state) => state.todolists);
 
     useEffect(() => {
-        dispatch(getOneToDoListResources(parseInt(id!)));
+        dispatch(getOneToDoListResources(parseInt(id!))).then(() => {
+            setLoading(false);
+        });
     }, []);
 
     const handleUpdateStatus = (val: boolean) => {
         dispatch(updateToDoListResources(parseInt(id!), !val))
     }
 
+    function Loading() {
+        return <div className="text-center">loading...</div>;
+    }
+
     return (
         <div className="max-w-screen-xl mx-auto p-4 ">
-            <div className="flex gap-2 cursor-pointer mt-4 mb-10" onClick={() => router.back()}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="black" d="M9.308 17.308L4 12l5.308-5.308l.707.708l-4.1 4.1H20v1H5.915l4.1 4.1z" /></svg>Back</div>
-            <div className="mx-auto w-6/12 p-16 gap-2 border text-center rounded">
-                <p>{todoTask.todo}</p>
-                <button onClick={() => handleUpdateStatus(todoTask?.completed)} className={getStatus(todoTask?.completed).color}>{getStatus(todoTask?.completed).text}</button>
-            </div>
+            {loading ? <Loading /> : <> <div className="flex gap-2 cursor-pointer mt-4 mb-10" onClick={() => router.back()}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="black" d="M9.308 17.308L4 12l5.308-5.308l.707.708l-4.1 4.1H20v1H5.915l4.1 4.1z" /></svg>Back</div>
+                <div className="mx-auto w-6/12 p-16 gap-2 border text-center rounded">
+                    <p>{todoTask.todo}</p>
+                    <button onClick={() => handleUpdateStatus(todoTask?.completed)} className={getStatus(todoTask?.completed).color}>{getStatus(todoTask?.completed).text}</button>
+                </div></>
+            }
         </div>)
 }
 
