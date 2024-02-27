b5490f3a4a5295eb358fe8829a8b440074815b5a
monethip <monethip1299@gmail.com>
2024-02-27 17:39:57 +0700

    store skip in url

--- src/app/(primary)/page.tsx
+++ src/app/(primary)/page.tsx
@@ -5,40 +5,37 @@ import Pagination from "../../components/elements/Pagination";
 import { getResources } from "@/redux/todolist";
 import { useSelector, useDispatch } from '@/redux/store';
 import getStatus from "@/utils/status";
-import { useRouter, useSearchParams, usePathname } from 'next/navigation';
-import { useCallback } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function MainPage() {
     const dispatch = useDispatch();
     const router = useRouter();
-    const [currentPage, setCurrentPage] = useState(1);
+    const searchParams = useSearchParams();
     const [skip, setSkip] = useState(0);
     const [loading, setLoading] = useState(true);
-    const pathName = usePathname();
 
     const { data } = useSelector((state) => state.todolists);
 
-    const limit = data?.limit
-    const total = data?.total
-
-
-    //
-    const paginate = (pageNumber: number) => {
-        const newSkip = (pageNumber - 1) * limit;
-        setSkip(newSkip);
-        setCurrentPage(pageNumber);
-        dispatch(getResources({ limit, skip: newSkip })).then(() => {
-            setLoading(false);
-        });
-        // router.push(pathName + '?' + createQueryString('limit', limit.toString()))
-        window.history.pushState(null, '', `?skip=${newSkip.toString()}`);
-    };
+    const search = parseInt(searchParams?.get('skip') as string) || 0;
+    const [currentPage, setCurrentPage] = useState(Math.ceil((search) / data?.limit) + 1);
 
     useEffect(() => {
-        paginate(currentPage);
-
-
+        dispatch(getResources({ limit: data?.limit, skip: (currentPage - 1) * data?.limit })).then(() => {
+            setLoading(false);
+        });
     }, [currentPage]);
+
+
+    const paginate = (pageNumber: number) => {
+        const newSkip = (pageNumber - 1) * data?.limit;
+        setSkip(newSkip);
+        setCurrentPage(pageNumber);
+
+        const params = new URLSearchParams(searchParams?.toString())
+        params.set('skip', newSkip.toString())
+        window.history.pushState(null, '', `?limit=${data?.limit}&skip=${newSkip.toString()}`);
+    };
+
 
     const handleToDoDetail = (id: number) => {
         router.push(`/to-do/${id}`)
@@ -58,7 +55,7 @@ export default function MainPage() {
                             <div onClick={(() => handleToDoDetail(item.id))} className={getStatus(item.completed).textColor} key={index}>{item.todo}</div>
                         ))}
                     </div>
-                    <Pagination currentPage={currentPage} skip={skip} limit={limit} total={total} paginate={paginate} />
+                    <Pagination currentPage={currentPage} skip={skip} limit={data?.limit} total={data?.total} paginate={paginate} />
                 </>
             }
         </div>
--- src/utils/status.ts
+++ src/utils/status.ts
@@ -1,7 +1,8 @@
 export default function getStatus(completed: boolean): { color: string, textColor: string, text: string } {
     if (completed) {
-        return { color: 'mt-4 px-4 py-2 bg-green-500 text-white rounded', textColor: 'border p-4 cursor-pointer rounded text-green-500', text: 'Done' };
+        return { color: 'mt-4 px-4 py-2 bg-red-500 text-white rounded', textColor: 'border p-4 cursor-pointer rounded text-green-500', text: 'INCOMPLETE' };
     } else {
-        return { color: 'mt-4 px-4 py-2 bg-primary text-white rounded', textColor: 'border p-4 cursor-pointer rouunded', text: 'Incomplete' };
+        return { color: 'mt-4 px-4 py-2 bg-green-500 text-white rounded', textColor: 'border p-4 cursor-pointer rouunded', text: 'COMPLETED' };
+
     }
 }
